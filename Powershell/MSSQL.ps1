2d8809bb1db48269e424bd9d46748c6a5f24a22b
mlxonunez <net.oscar.nunez@outlook.com>
2021-09-28 09:29:19 +0200

    Add default structure ps1 and MSSQL function query
    Raw first commit from MSSQL. Must be converted into an object to obtain more easy the query result, as it is done on Python

---
+++ Powershell/MSSQL.ps1
@@ -0,0 +1,58 @@
+<#
+.Synopsis
+   Execute sql server query using Domain user
+.NOTES
+   Additional Notes, eg 
+   File Name  : MSSQL.ps1 
+   Author     : Óscar Núñez - net.oscar.nunez@outlook.com>
+   Requires   : PowerShell V5
+   Appears in -full  
+.EXAMPLE
+   doQuery -dbIdentity (Get-Credential) -dbQuery "SELECT GETDATE()" -dbName "database_name" -dbServer "servername.domain.com"
+#>
+
+# Functions
+Function doQuery (){
+    param(
+            $dbServer,
+            $dbIdentity,
+            $dbQuery,
+            $dbName
+        )
+
+    $sqlusername    = $dbIdentity[0]
+	$sqlpassword    = $dbIdentity[1]
+	
+  # Set Object to Return
+    $array = [System.Collections.ArrayList]::new() 
+
+  # Open SQL Connection
+    $connSS = "Server=$dbServer;Database=$dbName;UID=$sqlusername;PWD=$sqlpassword;Integrated Security=true;"
+    $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+    $SqlConnection.ConnectionString = $connSS
+    $SqlConnection.Open()
+
+  # Prepare Query
+    $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+    $SqlCmd.CommandText = $dbQuery
+
+    $SqlCmd.Connection = $SqlConnection
+    $reader = $SqlCmd.ExecuteReader()
+
+  # Read the result of reader
+    while ($reader.Read())
+    {
+        $x = New-Object -TypeName PSObject
+        for ($i = 0; $i -lt $reader.FieldCount; ++$i)
+        {
+		    $x | Add-Member -Type NoteProperty -Name $reader.GetName($i) -Value $reader[$i]
+	    }
+        $array += $x
+    }
+    
+  # Close connection once done
+    $SqlConnection.Close()
+
+  # Return SQL Result inside objarray *PSObject
+    return $array
+}
---
+++ Powershell/defaultStructure.ps1
@@ -0,0 +1,37 @@
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+.NOTES
+   General notes
+   Additional Notes, eg 
+   File Name  : <yourfilename>.ps1 
+   Author     : <Yor Name> - <yourmail@domain.com>
+   Requires   : PowerShell V<versionCode>
+   Appears in -full  
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.PARAMETER <custom>
+	Parameter <custom> accepts only <k> for K value, <y> for Y value
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+
+##Start Script
+
+#Standar libraries
+
+#3rd party libraries
+
+#Local source
